267ab511ede2620180bb5f7a9d608df402491554
yuvalste13 <yuvalste13@gmail.com>
2022-11-10 19:45:03 +0200

    change

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import registerNNPushToken from 'native-notify';
+
+import Home from './src/screens/Home';
+import ChosenTask from './src/screens/ChosenTask';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  // push notifications
+  registerNNPushToken(your-app-id, 'your-app-token');
+    // get App ID and App Token from NativeNotify.com
+
+  // globalstate management
+  const [toDoList, setToDoList] = useState([{ id: 1, task: 'brush your teeth' }]);
+  const [task, setTask] = useState('');
+  const [chosenTask, setChosenTask] = useState('');
+
+  const GlobalState = {
+    toDoList, setToDoList,
+    task, setTask,
+    chosenTask, setChosenTask
+  }
+
+  // navigation
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home" options={{ headerShown: false }}>
+          {props => <Home {...props} GlobalState={GlobalState} />}
+        </Stack.Screen>
+
+        <Stack.Screen name="ChosenTask" options={{ headerShown: false }}>
+          {props => <ChosenTask {...props} GlobalState={GlobalState} />}
+        </Stack.Screen>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/ChosenTask.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
+
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+export default function ChosenTask({ navigation, GlobalState }) {
+    const { chosenTask } = GlobalState;
+
+    return (
+        <View style={styles.screen}>
+            <Header />
+            <View style={styles.body}>
+                <Text>{chosenTask.task}</Text>
+            </View>
+            <Footer navigation={navigation} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    body: {
+        flex: 8,
+        width: '100%',
+        backgroundColor: '#14141410',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
